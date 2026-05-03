afb2025d98f7cefb1d25d8154f96096b78f10c40
Michel Mileski <michel.mileski@gmail.com>
2026-05-03 16:05:55 -0300

    feat: application entry point and route definitions

---
+++ public/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Router;
+use App\Controllers\TransportadoraController;
+use App\Controllers\EntregaController;
+
+header('Content-Type: application/json; charset=utf-8');
+
+function json(mixed $data, int $status = 200): never
+{
+    http_response_code($status);
+    echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    exit;
+}
+
+function body(): array
+{
+    return json_decode(file_get_contents('php://input'), true) ?? [];
+}
+
+$router = new Router();
+
+// Transportadoras
+$router->get('/transportadoras',                  [TransportadoraController::class, 'index']);
+$router->post('/transportadoras',                 [TransportadoraController::class, 'store']);
+$router->get('/transportadoras/{id}',             [TransportadoraController::class, 'show']);
+$router->patch('/transportadoras/{id}/desativar', [TransportadoraController::class, 'desativar']);
+$router->patch('/transportadoras/{id}/reativar',  [TransportadoraController::class, 'reativar']);
+
+// Entregas
+$router->get('/entregas',               [EntregaController::class, 'index']);
+$router->post('/entregas',              [EntregaController::class, 'store']);
+$router->get('/entregas/{id}',          [EntregaController::class, 'show']);
+$router->patch('/entregas/{id}/status', [EntregaController::class, 'updateStatus']);
+
+$router->dispatch();
